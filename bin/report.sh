c8d91a9e3eca36485ae780b376edbb41ccd2f576
Shane Ramey <shane.ramey@gmail.com>
2017-05-26 01:45:01 -0700

    report.sh script initial version

---
+++ bin/report.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+MINIKUBE_IP=`minikube ip`
+echo "cAdvisor: http://$MINIKUBE_IP:4194"
+
+echo "to trust the minikube CA cert, run (MacOS):"
+echo sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ~/.minikube/ca.crt
+
+echo "Helm Release Notes"
+for release in `helm list -q`; do
+	echo "-----" &&
+	echo "Release: $release" &&
+	helm status $release
+done
