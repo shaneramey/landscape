9368cc17213c2f70bebc428198e42efcc4cba5db
Shane Ramey <shane.ramey@gmail.com>
2017-06-01 23:27:25 -0700

    bin/report.sh: enable namespace selector

--- bin/report.sh
+++ bin/report.sh
@@ -1,13 +1,21 @@
 #! /usr/bin/env bash
 
 MINIKUBE_IP=`minikube ip`
+NAMESPACE=$1
 echo "cAdvisor: http://$MINIKUBE_IP:4194"
 
 echo "to trust the minikube CA cert, run (MacOS):"
 echo sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ~/.minikube/ca.crt
 
+helm_list_cmd="helm list -q"
+namespace_selector=""
+if ! [ "$NAMESPACE" == "__all_namespaces__" ]; then
+  namespace_selector=" --namespace=${NAMESPACE}"
+fi
+helm_list_cmd="$helm_list_cmd$namespace_selector"
+
 echo "Helm Release Notes"
-for release in `helm list -q`; do
+for release in `$helm_list_cmd`; do
     echo "-----" &&
     echo "Release: $release" &&
     helm status $release
