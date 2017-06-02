716c83e8e9aab0e0dd2f173e5beea349e57a7383
Shane Ramey <shane.ramey@gmail.com>
2017-06-02 02:24:01 -0700

    vault fix bash script syntax

--- bin/env-auth-vault.sh
+++ bin/env-auth-vault.sh
@@ -1,7 +1,7 @@
+#! /usr/bin/env bash
 GIT_BRANCH=`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`
 CLUSTER_DOMAIN=${GIT_BRANCH}.local
 
-#! /usr/bin/env bash
 # when this command is run from inside a k8s cluster, it should use its own cluster vault
 if [ -z ${VAULT_ADDR+x} ]; then
     export VAULT_ADDR="https://http.vault.svc.${CLUSTER_DOMAIN}:8200"
