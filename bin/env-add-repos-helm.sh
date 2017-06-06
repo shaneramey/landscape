28711763f6f56df2e73beac163251999540b23fd
Shane Ramey <shane.ramey@gmail.com>
2017-06-06 03:16:28 -0700

    deployment/ environment

--- bin/env-add-repos-helm.sh
+++ bin/env-add-repos-helm.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 # set up Helm chart repos. FIXME: move to initialization method
+helm init --client-only
 helm repo add charts.downup.us http://charts.downup.us
 helm repo remove local 2> /dev/null
 
--- bin/env-auth-vault.sh
+++ bin/env-auth-vault.sh
@@ -2,9 +2,10 @@
 GIT_BRANCH=`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`
 CLUSTER_DOMAIN=${GIT_BRANCH}.local
 
-# when this command is run from inside a k8s cluster, it should use its own cluster vault
+# assume if VAULT_ADDR unset, we are running inside the cluster
+# set the k8s-distributed ca.crt
 if [ -z ${VAULT_ADDR+x} ]; then
     export VAULT_ADDR="https://http.vault.svc.${CLUSTER_DOMAIN}:8200"
+    export VAULT_CACERT="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 fi
-unset VAULT_TOKEN # auth doesnt work unless this is unset
 export VAULT_TOKEN=$(vault read -field id auth/token/lookup-self)
