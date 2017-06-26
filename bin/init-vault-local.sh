40e261987af10778afdeee2ec329f8fdb7c8b50e
Shane Ramey <shane.ramey@gmail.com>
2017-06-26 14:15:16 -0700

    add notes re: bootstrap

--- bin/init-vault-local.sh
+++ bin/init-vault-local.sh
@@ -12,3 +12,5 @@ fi
 
 sleep 3
 export VAULT_TOKEN=`docker logs dev-vault 2>&1 | grep 'Root Token' | tail -n 1 | awk '{ print $3 }'`
+export VAULT_ADDR=http://127.0.0.1:8200
+vault auth ${VAULT_TOKEN}
