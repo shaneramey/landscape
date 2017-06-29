76c07f6fe6e57e1ddb12bd0d623239ce271c26a3
Shane Ramey <shane.ramey@gmail.com>
2017-06-29 06:10:50 -0700

    terraform gce credentials

--- bin/env-cluster-terraform.sh
+++ bin/env-cluster-terraform.sh
@@ -1,10 +1,27 @@
 #! /usr/bin/env bash
 
+# initializes a terraform cluster.
+# if that's been done already, update the cluster
+#
+# Requires tfstate storage backend
+# to create:
+# export GOOGLE_APPLICATION_CREDENTIALS=downloaded-serviceaccount.json
+#terraform init  -backend-config 'bucket=something-something-1234' \
+#                -backend-config 'path=tfstate-dev-123456/master.tfstate' \
+#                -backend-config 'project=dev-123456'
+# - or possibly set (https://github.com/google/google-auth-library-ruby/issues/65#issuecomment-198532641) 
+# GOOGLE_CLIENT_ID=
+# GOOGLE_CLIENT_EMAIL=
+# GOOGLE_ACCOUNT_TYPE=
+# GOOGLE_PRIVATE_KEY= ?
+
 GIT_BRANCH=`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`
 
 # test syntax
-terraform validate var/terraform && \
+pushd var/terraform
+terraform validate && \
 # prepare config
-terraform plan -var="branch_name=${GIT_BRANCH}" var/terraform && \
+terraform plan -var="branch_name=${GIT_BRANCH}" && \
 # apply if no error above
-terraform apply -var="branch_name=${GIT_BRANCH}" var/terraform
+terraform apply -var="branch_name=${GIT_BRANCH}"
+popd
