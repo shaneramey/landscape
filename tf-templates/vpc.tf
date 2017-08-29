5199c87c778ed8c7f67a3cee58149194cbff4519
Shane Ramey <shane.ramey@gmail.com>
2017-08-29 12:35:37 -0700

    Revert "fix: add region to terraform google_compute_subnetwork"
    This reverts commit 255252a9762333edfe33f5ae7f90cee57c890c93.

--- tf-templates/vpc.tf
+++ tf-templates/vpc.tf
@@ -86,9 +86,8 @@ resource "google_compute_network" "networkA" {
 
 resource "google_compute_subnetwork" "gke_cluster1" {
   name       = "gke-master"
-  network = "${google_compute_network.networkA.self_link}"
-  region  = "${data.vault_generic_secret.gce_project_secrets.data["region"]}"
   ip_cidr_range = "${data.vault_generic_secret.gce_project_secrets.data["gke_network1_ipv4_cidr"]}"
+  network = "${google_compute_network.networkA.self_link}"
 }
 
 # Attach a VPN gateway to each network.
