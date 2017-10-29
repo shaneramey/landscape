83818f71e34aa224864a0e3d48e4bcdbe272b76e
Shane Ramey <shane.ramey@gmail.com>
2017-10-29 00:05:32 -0700

    fix: remove extra files from forked branch

--- landscape/test_kubernetes.py
+++
@@ -1,7 +0,0 @@
-from .kubernetes import provisioner_from_context_name
-
-def test_provisioner_from_context_name_minikube():
-	assert provisioner_from_context_name('minikube') == 'minikube'
-
-def test_provisioner_from_context_name_gke():
-	assert provisioner_from_context_name('gke_dummy') == 'terraform'
